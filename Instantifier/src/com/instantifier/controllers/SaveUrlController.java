ae9f48f076e03649f08a6964c54fb032ad70160f
adrian.ron <adrian.ronayne@gmail.com>
2012-02-23 23:55:11 +0000

    Added comment

--- Instantifier/src/com/instantifier/controllers/SaveUrlController.java
+++ Instantifier/src/com/instantifier/controllers/SaveUrlController.java
@@ -1,5 +1,8 @@
 package com.instantifier.controllers;
 
+/**
+ * test comment
+ */
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
